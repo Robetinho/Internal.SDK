b58ae05cdee101b652c1b2d5ff6da4c82b632c13
robert Beamish <robertbeamish@sonstream.com>
2025-12-15 04:12:30 +0000

    small change

--- Internal.SDK/SlackMessenger/SlackMessengerClient.cs
+++ Internal.SDK/SlackMessenger/SlackMessengerClient.cs
@@ -2,23 +2,23 @@
 using Internal.SDK.SlackMessenger.DTOs;
 using Internal.SDK.SystemLogger.DTOs;
 
-namespace Internal.SDK.SlackMessenger 
+namespace Internal.SDK.SlackMessenger
 {
 
     public class SlackMessengerClient : ClientBase, ISlackMessengerClient
-    {   
+    {
         public SlackMessengerClient(HttpClient httpClient) : base("http://localhost:5005/", "api/slack-messenger", httpClient) { }
-         
-        public SlackMessengerClient() : this(null!) { } 
-           
+
+        public SlackMessengerClient() : this(null!) { }
+
         public async Task<Response<SendSlackMessageResponseDTO>> SendSlackMessage(SendSlackMessageRequestDTO Payload)
         {
             return await GetPostResponse<SendSlackMessageResponseDTO>("sendSlackMessage", Payload);
         }
 
-        public void SendSlackMessage(string slackChannelName, string message)
+        public async Task<bool> SendSlackMessage(string slackChannelName, string message)
         {
-            _ = Task.Run(() => SendSlackMessage(new SendSlackMessageRequestDTO {  Message = message, SlackChannelName = slackChannelName} ));
+            return (await SendSlackMessage(new SendSlackMessageRequestDTO { Message = message, SlackChannelName = slackChannelName })).Item.Success;
         }
     }
 }
