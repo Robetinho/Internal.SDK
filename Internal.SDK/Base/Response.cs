7c59d189a2f09d70e3eb2d96d9554351c542e11f
robert Beamish <robertbeamish@sonstream.com>
2025-12-16 16:08:11 +0000

    .

--- Internal.SDK/Base/Response.cs
+++ Internal.SDK/Base/Response.cs
@@ -1,6 +1,4 @@
-﻿ 
-
-namespace Internal.SDK.Base
+﻿namespace Internal.SDK.Base
 {
     public class Response<T>
     {
@@ -19,35 +17,35 @@ namespace Internal.SDK.Base
 
         internal string? _queryString { get; init; }
 
-        internal ClientBase? _clientBase { get; init; } 
+        internal ClientBase? _clientBase { get; init; }
 
-        public async Task<Response<T>> RetryIfFailed(int retryAttemps = 1, int waitTime = 100)
+        public async Task RetryIfFailed(int retryAttemps = 1, int waitTime = 100)
         {
-            if (this.IsSuccess)
+            if (!this.IsSuccess)
             {
-                return this;
-            } 
-             
-            Response<T> lastResponse = this;
-            
-            for (int attempt = 0; attempt < retryAttemps; attempt++)
-            {
-                await Task.Delay(waitTime);
-                
-                lastResponse = await _clientBase!.GetResponse<T>(_httpMethod!, _path!, _body!, _queryString!);
-                
-                if (lastResponse.IsSuccess)
-                {
-                    if (_clientBase._systemLoggerClient != null)
-                    {
-                        _clientBase._systemLoggerClient.LogAlert($"The retry attemp of the call: {_httpMethod!} {_path} Body: {_body} QueryString: {_queryString} executed successfully");
-                    }
 
-                    return lastResponse;
+                Response<T> lastResponse = this;
+
+                for (int attempt = 0; attempt < retryAttemps; attempt++)
+                {
+                    await Task.Delay(waitTime);
+
+                    lastResponse = await _clientBase!.GetResponse<T>(_httpMethod!, _path!, _body!, _queryString!);
+
+                    if (lastResponse.IsSuccess)
+                    {
+                        if (_clientBase._systemLoggerClient != null)
+                        {
+                            _clientBase._systemLoggerClient.LogAlert($"The retry attemp of the call: {_httpMethod!} {_path} Body: {_body} QueryString: {_queryString} executed successfully");
+                        }
+                        break;
+                    }
                 }
+
+                this.Item = lastResponse.Item;
+                this.IsSuccess = lastResponse.IsSuccess;
+                this.ResponseCode = lastResponse.ResponseCode;
             }
-            
-            return lastResponse;
-        } 
-    } 
+        }
+    }
 }
