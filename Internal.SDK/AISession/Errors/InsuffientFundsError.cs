ab354eb557fa4aff346ea6decf28d36c902b19cb
robert Beamish <robertbeamish@sonstream.com>
2026-02-26 23:12:51 +0000

    Added a few error types

---
+++ Internal.SDK/AISession/Errors/InsuffientFundsError.cs
@@ -0,0 +1,20 @@
+﻿using Internal.SDK.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internal.SDK.AISession
+{
+    public class InvalidModelError : AISessionError
+    {
+        public InvalidModelError(string modelName) : base($"The model {modelName} does not exist")
+        { 
+            ModelName = modelName;
+        }
+        
+        public string ModelName { get; init; }
+
+    }
+}
---
+++ Internal.SDK/AISession/Errors/InvalidModelError.cs
@@ -0,0 +1,23 @@
+﻿using Internal.SDK.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internal.SDK.AISession
+{
+    public class InsuffientFundsError : AISessionError
+    {
+        public InsuffientFundsError(int requestCost, int availableFunds) : base("Insufficient funds available")
+        {
+            RequestCost = requestCost;
+            AvailableFunds = availableFunds;
+        }
+
+        public int RequestCost { get; init; }
+
+        public int AvailableFunds { get; init; }
+
+    }
+}
