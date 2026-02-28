a7c45b2cf22805ffd284a6a7fedd3017c5c8afea
robert Beamish <robertbeamish@sonstream.com>
2026-02-28 21:55:45 +0000

    asdfasd

--- Internal.SDK/CorePractice/CorePracticeClient.cs
+++ Internal.SDK/CorePractice/CorePracticeClient.cs
@@ -8,7 +8,7 @@ namespace Internal.SDK.CorePractice
 
     public class CorePracticeClient : ClientBase<CorePracticeError>, ICorePracticeClient
     {
-        public CorePracticeClient(HttpClient? httpClient = null, ISystemLoggerClient? systemLoggerClient = null) : base("http://localhost:5007/", "api/ai-session", httpClient, systemLoggerClient) { }
+        public CorePracticeClient(HttpClient? httpClient = null, ISystemLoggerClient? systemLoggerClient = null) : base("http://localhost:5007/", "api/core-practice", httpClient, systemLoggerClient) { }
 
         public async Task<Response<bool, CorePracticeError>> DeleteClient(ClientDTO Payload)
         {
