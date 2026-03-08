5a1fb6e87048a67d87841dbf564ccb645e3f4916
robert Beamish <robertbeamish@sonstream.com>
2026-03-08 22:02:20 +0000

    sdfgds

--- Internal.SDK/CorePractice/DTOs/CalendarDTO.cs
+++ Internal.SDK/CorePractice/DTOs/CalendarDTO.cs
@@ -6,53 +6,53 @@ using System.Threading.Tasks;
 
 namespace Internal.SDK.CorePractice.DTOs
 {
-    public class ServiceDTO : ServiceSummaryDTO
+    public class CalendarDTO
     { 
 
-            public string? Explanation { get; init; }
+            //public string? Explanation { get; init; }
 
-            public string? Group { get; init; }
+            //public string? Group { get; init; }
           
-            public int? Order { get; init; }
+            //public int? Order { get; init; }
 
-            public int? Type { get; init; }
+            //public int? Type { get; init; }
 
-            public int? Estimate { get; init; }
+            //public int? Estimate { get; init; }
 
-            public string? Mode { get; init; } 
+            //public string? Mode { get; init; } 
 
-            public string? ModeValue { get; init; }
+            //public string? ModeValue { get; init; }
 
-            public bool? RequireApproval { get; init; }
+            //public bool? RequireApproval { get; init; }
 
-            public bool? IsInternalBooking { get; init; }
+            //public bool? IsInternalBooking { get; init; }
 
-            public bool? IsOnlineBooking { get; init; }
+            //public bool? IsOnlineBooking { get; init; }
 
-            public bool? IsActive { get; init; }
+            //public bool? IsActive { get; init; }
 
-            public string? EventKey { get; init; }
+            //public string? EventKey { get; init; }
 
-            public string? OnlineServiceKey { get; init; }
+            //public string? OnlineServiceKey { get; init; }
 
-            public bool? IsTop { get; init; }
+            //public bool? IsTop { get; init; }
 
-            public bool? IsFirstReminder { get; init; }
+            //public bool? IsFirstReminder { get; init; }
 
-            public bool? IsSecondReminder { get; init; }
+            //public bool? IsSecondReminder { get; init; }
 
-            public bool? IsResendReminder { get; init; }
+            //public bool? IsResendReminder { get; init; }
 
-            public DateTime? StartDate { get; init; }
+            //public DateTime? StartDate { get; init; }
 
-            public DateTime? EndDate { get; init; }
+            //public DateTime? EndDate { get; init; }
 
-            public object? Item { get; init; }
+            //public object? Item { get; init; }
 
-            public int? OnlinePaymentType { get; init; }
+            //public int? OnlinePaymentType { get; init; }
 
-            public string? DisplayPrice { get; init; }
+            //public string? DisplayPrice { get; init; }
 
-            public decimal? Amount { get; init; } 
+            //public decimal? Amount { get; init; } 
     }
 }
