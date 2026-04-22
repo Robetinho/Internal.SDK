778049cbcd8ee4edc6b78464f00ead195abb1fef
robert Beamish <robertbeamish@sonstream.com>
2026-04-22 07:52:26 +0100

    added 3rd party connection error type

---
+++ Internal.SDK/TextToSpeech/Errors/ThirdPartyConnectionError.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internal.SDK.TextToSpeech.Errors
+{
+    public class ThirdPartyConnectionError : TextToSpeechError
+    {  
+        public required string ApiName { get; init; }
+    }
+}
