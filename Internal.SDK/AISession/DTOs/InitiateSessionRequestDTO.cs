3e5e566bdf7adccc601e9cec3c182685b2b7b860
robert Beamish <robertbeamish@sonstream.com>
2026-04-28 18:19:11 +0100

    fixed a dto issue

--- Internal.SDK/AISession/DTOs/InitiateSessionRequestDTO.cs
+++ Internal.SDK/AISession/DTOs/InitiateSessionRequestDTO.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Internal.SDK.AISession.DTOs
 {
-    public class InitiateSessionRequestDTO
+    public class InitiateSessionRequestDTO : GetSingleReplyRequestDTO
     {
          
         public Guid? SessionId { get; init; }
