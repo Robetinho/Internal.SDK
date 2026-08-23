09d96a80742ffd991f549c0efad92db02059f231
robert Beamish <robertbeamish@sonstream.com>
2026-08-23 01:46:07 +0100

    fixed wrong url

--- Internal.SDK/EmailTemplate/EmailClientBase.cs
+++ Internal.SDK/EmailTemplate/EmailClientBase.cs
@@ -8,7 +8,7 @@ namespace Internal.SDK.Email
 
     public class EmailClientBase : ClientBase<EmailError>, IEmailClient
     {
-        public EmailClientBase(string domain, HttpClient? httpClient = null, ISystemLoggerClient? systemLoggerClient = null) : base(domain, "api/ai-session", httpClient, systemLoggerClient) { }
+        public EmailClientBase(string domain, HttpClient? httpClient = null, ISystemLoggerClient? systemLoggerClient = null) : base(domain, "api/email", httpClient, systemLoggerClient) { }
 
         public async Task<Response<bool, EmailError>> SendEmail(EmailDto Payload)
         {
--- Internal.SDK/__Template/__TemplateClientBase.cs
+++ Internal.SDK/__Template/__TemplateClientBase.cs
@@ -8,7 +8,8 @@ namespace Internal.SDK.__Template
 
     public class __TemplateClientBase : ClientBase<__TemplateError>, I__TemplateClient
     {
-        public __TemplateClientBase(string domain, HttpClient? httpClient = null, ISystemLoggerClient? systemLoggerClient = null) : base(domain, "api/ai-session", httpClient, systemLoggerClient) { }
+        public __TemplateClientBase(string domain, HttpClient? httpClient = null, ISystemLoggerClient? systemLoggerClient = null) 
+            : base(domain, "api/ai-session", httpClient, systemLoggerClient) { }
 
         public async Task<Response<string, __TemplateError>> GetReply(MessageDto Payload)
         {
